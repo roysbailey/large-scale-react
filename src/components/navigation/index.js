ba49ba512fb27c8b3cb105a725167683dfaeb5f4
James King <hello@jamesmichaelking.com>
2022-12-07 14:24:28 +0000

    Use react router link methods

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,12 +1,12 @@
-
+import { Link } from "react-router-dom";
 
 export const Navigation = () => {
     return (
         <nav>
             <ul>
-                <li><a href="/">Dashboard</a> </li>
-                <li><a href="/health-issues">Health issues</a> </li>
-                <li><a href="/incident-analysis">Incident analysis</a> </li>
+                <li><Link to={`/`}>Dashboard</Link> </li>
+                <li><Link to={`/health-issues`}>Health issues</Link> </li>
+                <li><Link to={`/incident-analysis`}>Incident analysis</Link> </li>
             </ul>
         </nav>
     )
