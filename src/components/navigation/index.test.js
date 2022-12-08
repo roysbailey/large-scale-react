a0072be5f4cad518f5adb2c736459b0406971fb0
James King <hello@jamesmichaelking.com>
2022-12-08 09:18:19 +0000

    Simple UI test

---
+++ src/components/navigation/index.test.js
@@ -0,0 +1,16 @@
+import { Navigation } from './Navigation';
+import { Link } from 'react-router-dom';
+
+jest.mock('react-router-dom', () => ({
+    Link: () => <div />,
+}));
+
+test('renders a navigation bar with three links', () => {
+    const wrapper = shallow(<Navigation />);
+    expect(wrapper.find('nav')).toHaveLength(1);
+    expect(wrapper.find('ul')).toHaveLength(1);
+    expect(wrapper.find('li')).toHaveLength(3);
+    expect(wrapper.find('li').at(0).find(Link).props().to).toBe('/');
+    expect(wrapper.find('li').at(1).find(Link).props().to).toBe('/health-issues');
+    expect(wrapper.find('li').at(2).find(Link).props().to).toBe('/incident-analysis');
+});
